0b22e5a1973a68c0ef11c0ba711ccccf56cb40e6
Kevin <kevin.huard@viacesi.fr>
2018-12-07 14:50:05 +0100

    feature : sqlconnector functions added
    Function Connect/Disconnect to use the db

--- Kitchen_Simulation/Models/SQLConnector.cs
+++ Kitchen_Simulation/Models/SQLConnector.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Kitchen_Simulation.Models
 {
     class SQLConnector
     {
+        internal static SQLConnector Instance { get; set; }
+
+        public void Connect(string db)
+        {
+            string cnx = "Data Source='" + db + "';Initial Catalog=DB_A2_WS2;Integrated Security=SSPI;";
+            SqlConnection SqlConnection = new SqlConnection();
+            SqlCommand sqlCommand = new SqlCommand();
+            SqlConnection.ConnectionString = cnx;
+            SqlConnection.Open();
+        }
+
+        public void Disconnect(SqlConnection connection)
+        {
+            connection.Close();
+        }
     }
 }

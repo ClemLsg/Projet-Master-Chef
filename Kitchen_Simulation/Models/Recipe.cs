9f5c3a37784c4f783d1d8503b838ddfe2161b765
Kevin <kevin.huard@viacesi.fr>
2018-12-07 14:04:22 +0100

    feature : recipe and recipe types functions added

---
+++ Kitchen_Simulation/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen_Simulation.Models
+{
+    class Recipe
+    {
+
+        public string Name { get; set; }
+        internal RecipeType Type { get; set; }
+        internal List<Ingredient> Ingredients { get; set; }
+    }
+}

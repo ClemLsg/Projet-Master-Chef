53b63139c4e1078344cfa9cc437b9fd417bd7b00
Kevin <kevin.huard@viacesi.fr>
2018-12-07 14:05:18 +0100

    feature : cook functions added

--- Kitchen_Simulation/Models/Cook.cs
+++ Kitchen_Simulation/Models/Cook.cs
@@ -8,5 +8,24 @@ namespace Kitchen_Simulation.Models
 {
     class Cook
     {
+        public void GetTool()
+        {
+
+        }
+
+        public void FreeTool()
+        {
+
+        }
+
+        public void BringTool()
+        {
+
+        }
+
+        public List<Ingredient> AskIngredient(Recipe recipe)
+        {
+            return recipe.Ingredients;
+        }
     }
 }

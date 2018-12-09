969dff77b65c821b0bce6c8f4a2dd8aab17a11e6
Dorian <dodo800882@gmail.com>
2018-12-09 16:23:41 +0100

    feat: add menu

--- Room_Simulation/Models/Menu.cs
+++ Room_Simulation/Models/Menu.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Room_Simulation.Models.Recipes;
 
 namespace Room_Simulation.Models
 {
     public class Menu
     {
-        public List<Recipe> Recipes { get; set; }
-        public Recipe MealOfTheDay { get; set; }
+        private static Menu _instance = null; // Menu instance
+        public static Menu Instance => _instance ?? (_instance = new Menu()); //Singleton
+
+        public List<Recipe> Recipes { get; } = new List<Recipe>();
+        public Recipe MealOfTheDay { get; }
         
-        public Menu(List<Recipe> recipes, Recipe mealOfTheDay)
+        private Menu()
         {
-            this.Recipes = recipes;
-            this.MealOfTheDay = mealOfTheDay;
+            this.CreateMenu();
+            
+            this.MealOfTheDay = Recipes[(new Random()).Next(0, Recipes.Count)];
+        }
+
+        private void CreateMenu()
+        {
+            List<Recipe> recipes = Recipe.Recipes;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Random randomGen = new Random();
+                int random = randomGen.Next(0, recipes.Count);
+                
+                this.Recipes.Add(recipes[random]);
+                recipes.Remove(recipes[random]);
+
+                if (recipes.Count <= 0)
+                    break;
+            }
         }
     }
 }

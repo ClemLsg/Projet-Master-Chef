bd269dd9c344a92b217fffcf124d38fc703e1b10
Kevin <kevin.huard@viacesi.fr>
2018-12-10 16:33:22 +0100

    feat : room tests created

---
+++ RoomTests/MenuTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Room_Simulation.Models;
+
+namespace RoomTests
+{
+    [TestClass]
+    public class MenuTests
+    {
+        Room_Simulation.Models.Recipes.PateDePorc pateDePorc = new Room_Simulation.Models.Recipes.PateDePorc();
+        Room_Simulation.Models.Recipes.Gaspatcho gaspaccio = new Room_Simulation.Models.Recipes.Gaspatcho();
+        Room_Simulation.Models.Recipes.OeufsCocotte oeufsCocotte = new Room_Simulation.Models.Recipes.OeufsCocotte();
+
+
+        [TestMethod]
+        public void SetMealOfTheDay()
+        {
+            if (Menu.Instance.MealOfTheDay.ToString() == pateDePorc.ToString())
+            {
+                Assert.AreEqual(pateDePorc.GetType().ToString(), Menu.Instance.MealOfTheDay.GetType().ToString());
+            }
+            else
+            {
+                if(Menu.Instance.MealOfTheDay.ToString() == gaspaccio.ToString())
+                {
+                    Assert.AreEqual(gaspaccio.GetType().ToString(), Menu.Instance.MealOfTheDay.GetType().ToString());
+                }
+                else
+                {
+                    if(Menu.Instance.MealOfTheDay.ToString() == oeufsCocotte.ToString())
+                    {
+                        Assert.AreEqual(oeufsCocotte.GetType().ToString(), Menu.Instance.MealOfTheDay.GetType().ToString());
+                    }
+                    else
+                    {
+                        Assert.AreEqual("menu not find", Menu.Instance.MealOfTheDay.GetType().ToString());
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ RoomTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoomTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoomTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af7857d8-3f89-4c38-96bd-2eb7110f7aa7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

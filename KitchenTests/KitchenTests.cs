ac60c9ddd1284c7c98b55bd4542f418ea82c142d
Kevin <kevin.huard@viacesi.fr>
2018-12-10 11:12:23 +0100

    feat : Test KitchenClerk

--- KitchenTests/KitchenTests.cs
+++ KitchenTests/KitchenTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Kitchen_Simulation.Models;
+using Kitchen_Simulation.Models.Ingredients;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
@@ -10,17 +11,27 @@ namespace KitchenTests
     [TestClass]
     public class KitchenClerkTests
     {
+        KitchenClerk kitchenClerk = new KitchenClerk();
 
-        [TestMethod]
-        public void pellVegetable()
-        {
-        }
-
+        
         [TestMethod]
         public void getIngredients()
         {
-
             
+            Sel salt = new Sel();
+            Tomate tomato = new Tomate();
+            Oeuf egg = new Oeuf();
+
+            List<Ingredient> Ingredients = new List<Ingredient>();
+            Ingredients.Add(salt);
+            Ingredients.Add(tomato);
+            Ingredients.Add(egg);
+
+            kitchenClerk.IngredientsList.Add(salt);
+            kitchenClerk.IngredientsList.Add(tomato);
+            kitchenClerk.IngredientsList.Add(egg);
+
+            Assert.AreEqual(Ingredients, kitchenClerk.IngredientsList);
 
 
 

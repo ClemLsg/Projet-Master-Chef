1b07cc4841ca55f1f9d5de16049df146dcc774ec
Kevin <kevin.huard@viacesi.fr>
2018-12-10 13:25:22 +0100

    fix : Add System.Data to Room

--- KitchenTests/SqlConnectorTests.cs
+++ KitchenTests/SqlConnectorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Kitchen_Simulation.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
 
 namespace KitchenTests
 {
@@ -12,6 +13,19 @@ namespace KitchenTests
         {
             SQLConnector sqlconnector = new SQLConnector();
             Assert.AreEqual(true,sqlconnector.Connect("KEVIN\\SQLEXPRESS"));
+        }
+
+        [TestMethod]
+        public void testDisconnect()
+        {
+            SQLConnector sqlconnector = new SQLConnector();
+            string db = "KEVIN\\SQLEXPRESS";
+            string cnx = "Data Source='" + db + "';Initial Catalog=DB_A2_WS2;Integrated Security=SSPI;";
+            SqlConnection SqlConnection = new SqlConnection();
+            SqlCommand sqlCommand = new SqlCommand();
+            SqlConnection.ConnectionString = cnx;
+            SqlConnection.Open();
+            Assert.AreEqual(true, sqlconnector.Disconnect(SqlConnection));
         }
     }
 }

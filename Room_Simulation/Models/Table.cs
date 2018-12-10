027edd7cc2a65435481b82c788aa56917590f262
Kevin <kevin.huard@viacesi.fr>
2018-12-10 17:16:33 +0100

    feat : table functions added

--- Room_Simulation/Models/Table.cs
+++ Room_Simulation/Models/Table.cs
@@ -2,6 +2,19 @@ namespace Room_Simulation.Models
 {
     public class Table
     {
-        
+        private ClientGroup clients;
+        public int iDTable { get; set; }
+        public int seats { get; set; }
+        public bool hasBread { get; set; }
+        public bool hasWater { get; set; }
+
+        public ClientGroup GetClients()
+        {
+            return this.clients;
+        }
+        public void AddClient(Client client)
+        {
+            clients.Clients.Add(client);
+        }
     }
 }

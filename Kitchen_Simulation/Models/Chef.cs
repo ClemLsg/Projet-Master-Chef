b8be6a42ac7a21391912a6233e095057f0210ac0
Unknown <clement.lesage@viacesi.fr>
2018-12-10 15:56:21 +0100

    feat: added observer on Chef

--- Kitchen_Simulation/Models/Chef.cs
+++ Kitchen_Simulation/Models/Chef.cs
@@ -10,16 +10,51 @@ namespace Kitchen_Simulation.Models
     public sealed class Chef
     {
         private static Chef _instance = null;
+        private List<Recipe> _recipePool = null;
+        private List<Cook> _waitingCooks = null;
 
         private Chef()
         {
+         
         }
 
         public static Chef Instance => _instance ?? (_instance = new Chef());
 
+        public void AddToPool(Recipe recipe)
+        {
+            this._recipePool.Add(recipe);
+        }
+
         public void AssignOrder(Cook cook, Recipe recipe)
         {
             cook.CookingRecipe = recipe;
+        }
+        
+        public void ReciveNewOrder(Order order)
+        {
+            AddToPool(order.Starter);
+            AddToPool(order.Main);
+            AddToPool(order.Dessert);
+            if (_waitingCooks.Any())
+            {
+                AssignOrder(_waitingCooks.First(), _recipePool.First());
+            }
+        }
+
+        public void OnCookNotify(string value, Cook c)
+        {
+            if (value == "Order completed")
+            {
+                if(_recipePool.Any())
+                {
+                    AssignOrder(c, _recipePool.First());
+                } else
+                {
+                    _waitingCooks.Add(c);
+                }
+                
+            }
+
         }
     }
 }

f00f78e1d0b997756982b2b64bd8a9db71c8615b
Unknown <clement.lesage@viacesi.fr>
2018-12-11 14:47:00 +0100

    feat: added controller to kitchen

---
+++ Kitchen_Simulation/Controllers/KitchenController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kitchen_Simulation.Models;
+
+namespace Kitchen_Simulation.Controllers
+{
+    public class KitchenController
+    {
+        private Main View;
+        private KitchenModel Model;
+
+        public KitchenController()
+        {
+            this.SetView(new Main());
+            this.SetModel(new KitchenModel(this));
+        }
+
+        public Main GetView()
+        {
+            return View;
+        }
+
+        public void SetView(Main value)
+        {
+            View = value;
+        }
+
+        public void Animation(string name)
+        {
+            this.GetView().GetSimulation().Animation(name);
+        }
+
+        public KitchenModel GetModel()
+        {
+            return Model;
+        }
+
+        public void SetModel(KitchenModel value)
+        {
+            Model = value;
+        }
+
+    }
+}

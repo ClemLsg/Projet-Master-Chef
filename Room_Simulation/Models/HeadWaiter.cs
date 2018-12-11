d4fc3dc599468c0ad901d734ee75fb2f7e8ff147
Unknown <ggraykas@gmail.com>
2018-12-11 10:32:03 +0100

    Add Buttler and headWaiter to Models Room
    Not Finish

---
+++ Room_Simulation/Models/HeadWaiter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room_Simulation.Models
+{
+    class HeadWaiter
+    {
+        Order ClientOrder = new Order();
+
+        public HeadWaiter( Square square )
+        {
+            //Constructor
+
+        }
+
+        public static void GiveMenu ()
+        {
+
+        }
+        public static TakeOrder(Client)
+        {
+            
+        }
+        public static BringOrder ()
+        {
+
+        }
+        public static void SwitchSquare(Square square)
+        {
+
+        }
+    }
+}
--- Room_Simulation/Models/Table.cs
+++ Room_Simulation/Models/Table.cs
@@ -1,7 +1,9 @@
 namespace Room_Simulation.Models
 {
+    
     public class Table
     {
+<<<<<<< Updated upstream
         private ClientGroup clients;
         public int iDTable { get; set; }
         public int seats { get; set; }
@@ -17,4 +19,62 @@ namespace Room_Simulation.Models
             clients.Clients.Add(client);
         }
     }
+=======
+
+        //FIELDS
+        private int IDTable;
+        private int Seats;
+        private bool HasBread;
+        private bool HasWater;
+        private bool IsCleaned;
+        ClientGroup Clients;
+
+        //CONSTRUCTOR
+        public Table(int place)
+        {
+            this.SetSeats(place);
+        }
+
+        //METHODS
+        public int GetIDTable()
+        {
+            return this.IDTable;
+        }
+
+        public void SetIDTable(int value)
+        {
+            this.IDTable = value;
+        }
+
+        public int GetSeats()
+        {
+            return Seats;
+        }
+
+        public void SetSeats(int value)
+        {
+            Seats = value;
+        }
+
+        public bool GetIsCleaned()
+        {
+            return IsCleaned;
+        }
+
+        public void SetIsCleaned(bool value)
+        {
+            IsCleaned = value;
+        }
+        public ClientGroup GetClients()
+        {
+            return Clients;
+        }
+        public void SetClients (ClientGroup value)
+        {
+            Clients = value;
+        }
+    }   
+
+
+>>>>>>> Stashed changes
 }
